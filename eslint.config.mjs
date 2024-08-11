afedb2e9cae4d38c0260799c9ecc28b26e4fd044
yangguangliang <yangguangliang@hetao101.com>
2024-08-11 13:03:14 +0800

    feat: add lint

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('eslint:recommended'),
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'], // 只检查 src 目录下的 TypeScript 文件
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'memberLike',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require', // 强制要求下划线开头
+        },
+      ],
+    },
+    env: {
+      browser: true, // 允许使用浏览器的全局变量
+    },
+  },
+];
