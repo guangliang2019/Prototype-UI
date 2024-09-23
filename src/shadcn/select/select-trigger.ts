cfcb1a1720fb9496b7021c03603ef08159c4bd3c
yangguangliang <yangguangliang@hetao101.com>
2024-09-23 13:04:57 +0800

    feat: shadcn-select (100%)

--- src/shadcn/select/select-trigger.ts
+++ src/shadcn/select/select-trigger.ts
@@ -1,5 +1,5 @@
 import { PrototypeSelectTrigger } from '@/prototype/select';
-import '@/lucide/chevrons-up-down';
+import './select-arrow';
 import { h } from '@/utils/dom';
 export default class ShadcnSelectTrigger extends PrototypeSelectTrigger {
   // 用户添加的 class 属性
@@ -17,8 +17,7 @@ export default class ShadcnSelectTrigger extends PrototypeSelectTrigger {
     this._computedClass =
       'cursor-pointer flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1';
 
-    // TODO: 这个图标目前看起来，和原版有点区别
-    this.appendChild(h('lucide-chevrons-up-down', { class: 'w-3.5 h-3.5 opacity-50' }));
+    this.appendChild(h('shadcn-select-arrow', { class: 'w-4 h-4 opacity-50' }));
 
     this.className = [this._computedClass, this._class].join(' ').trimEnd();
   }
