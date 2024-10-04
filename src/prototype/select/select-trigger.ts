9600a530e5f9d282362c5401bd3cfdbe1a79f2a3
255doesnotexist <1293883574jcy@gmail.com>
2024-10-05 01:59:02 +0800

    refactor: take out focusSelectedItem
    - and removed setTimeout logic

--- src/prototype/select/select-trigger.ts
+++ src/prototype/select/select-trigger.ts
@@ -6,34 +6,54 @@ export default class PrototypeSelectTrigger<
 > extends Trigger<T> {
   protected _consumerKeys = new Set(['prototype-select']);
 
-  private _handleMouseDown = () => {
+  private _handleMouseDown = (event: MouseEvent): void => {
     const context = this._contextValues['prototype-select'];
     context.width = this.offsetWidth;
-    if (document.activeElement === this) {
-      context.width = this.offsetWidth;
-      context.selecting ? context.close() : context.open();
 
-      const index = context.items.indexOf(context.value);
-      // 这里有些莫名其妙，如果不设置延时，在调用下列 focus 时，会触发当前元素的 _handleFocus 事件
-      // 猜测可能是与 open 时 item 刚刚插入进 DOM 树有关
-      // 或者是和 mousedown 与 focus 的触发顺序有关
-      // prettier-ignore
-      if (index !== -1) setTimeout(() => { context.itemsRefs[index].focus(); }, 0);
+    // Check if the click is on the trigger or its children
+    if (event.target instanceof Node && this.contains(event.target)) {
+      if (document.activeElement === this) {
+        context.selecting ? context.close() : context.open();
+
+        this._focusSelectedItem(context);
+      }
+    } else if (context.selecting) {
+      // If the click is outside and the select is open, close it
+      context.close();
     }
   };
 
-  private _handleKeydown = (event: KeyboardEvent) => {
-    if (event.key === 'Enter') this._handleFocus();
+  private _handleKeydown = (event: KeyboardEvent): void => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this._handleFocus();
+    }
   };
 
-  private _handleFocus = () => {
+  private _handleFocus = (): void => {
     const context = this._contextValues['prototype-select'];
     context.width = this.offsetWidth;
     context.selecting ? context.close() : context.open();
 
-    const index = context.items.indexOf(context.value);
-    if (index !== -1) context.itemsRefs[index].focus();
+    this._focusSelectedItem(context);
   };
+
+  private _focusSelectedItem(context: PrototypeSelectContext['prototype-select']): void {
+    const index = context.items.indexOf(context.value);
+    if (index !== -1 && Array.isArray(context.itemsRefs) && context.itemsRefs.length > index) {
+      requestAnimationFrame(() => {
+        const itemToFocus = context.itemsRefs[index];
+        if (itemToFocus && typeof itemToFocus.focus === 'function') {
+          itemToFocus.focus();
+        } else {
+          console.warn(`Failed to focus item at index ${index}. Item may not be properly initialized.`);
+        }
+      });
+    } else {
+      // -1 index appears when no item is selected, add this message for better debugging 
+      console.debug(`Invalid index ${index} or itemsRefs not properly initialized.`);
+    }
+  }
 
   connectedCallback() {
     super.connectedCallback();
@@ -41,15 +61,16 @@ export default class PrototypeSelectTrigger<
     context.triggerRef = this;
     context.focus = this.focus.bind(this);
 
-    this.addEventListener('mousedown', this._handleMouseDown);
+    this.addEventListener('mousedown', this._handleMouseDown as EventListener);
     this.addEventListener('focus', this._handleFocus);
 
     this.addEventListener('keydown', this._handleKeydown as EventListener);
   }
 
   disconnectedCallback() {
-    this.removeEventListener('mousedown', this._handleMouseDown);
+    this.removeEventListener('mousedown', this._handleMouseDown as EventListener);
     this.removeEventListener('focus', this._handleFocus);
+    this.removeEventListener('keydown', this._handleKeydown as EventListener);
   }
 }
 
