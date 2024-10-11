9f224a2069e8d86ba3f2bc7950c78b557014ab66
yangguangliang <yangguangliang@hetao101.com>
2024-10-11 13:15:48 +0800

    feat: 添加了 examples 的模版格式

--- src/www/app/examples/examples.ts
+++ src/www/app/examples/examples.ts
@@ -13,7 +13,7 @@ export default class ExamplesPage extends HTMLElement {
       'text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1] hidden md:block';
     const subTitleCls = 'max-w-2xl text-lg font-light text-foreground';
     fragment.appendChild(
-      h('div', { class: 'container relative' }, [
+      h('div', { class: 'container relative flex flex-col' }, [
         h('div', { class: bannerCls }, [
           h('h1', { class: titleCls }, ['Check out some examples']),
           h('p', { class: subTitleCls }, [
@@ -25,6 +25,28 @@ export default class ExamplesPage extends HTMLElement {
             h('shadcn-button', { variant: 'primary', class: 'h-8 px-3 text-xs' }, ['Get started']),
             h('shadcn-button', { variant: 'ghost', class: 'h-8 px-3 text-xs' }, ['Components']),
           ]),
+        ]),
+
+        h('div', { class: 'w-full h-[800px] rounded-[0.5rem] border bg-background shadow' }, [
+          h('iframe', { src: window.origin + '/examples/mail/index.html' }),
+        ]),
+        h('footer', { class: 'py-6 md:px-8 md:py-0' }, [
+          h(
+            'div',
+            {
+              class: 'flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row',
+            },
+            [
+              h(
+                'p',
+                {
+                  class:
+                    'text-balance text-center text-sm leading-loose text-muted-foreground md:text-left',
+                },
+                ['Build by 广亮, inspired by shadcn/ui, The source code is available on GitHub.']
+              ),
+            ]
+          ),
         ]),
       ])
     );
