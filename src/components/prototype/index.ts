76ca592fdb4e043741e8030ced1c0371014ddbb0
MrZhang <zq2012566134@gmail.com>
2025-03-16 17:57:17 +0800

    0

--- src/components/prototype/index.ts
+++ src/components/prototype/index.ts
@@ -22,5 +22,5 @@ export { PrototypeTransition } from './transition';
 export { PrototypeResizable, PrototypeResizableHandle, PrototypeResizablePanel } from './resizable';
 export { PrototypeTestTab, PrototypeTestTabTrigger, PrototypeTestTabContent } from './test-tab';
 export { PrototypeTestButton } from './test-button';
-export { PrototypeSwitch,  } from './new-switch';
+export { PrototypeSwitch} from './new-switch';
 
